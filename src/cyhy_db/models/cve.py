bf8c273bdecdcef784ce86b6d5ba5611f4deff63
Felddy <markf@geekpad.com>
2024-01-24 16:27:27 -0500

    Apply changes made by linters
    Add comments to severity calculation.

--- src/cyhy_db/models/cve.py
+++ src/cyhy_db/models/cve.py
@@ -1,24 +1,41 @@
-from odmantic import Model, Field
-from pydantic import ValidationInfo, field_validator
+# Standard Python Libraries
 from typing import Literal
+
+# Third-Party Libraries
+from odmantic import Field, Model
+from pydantic import ValidationInfo, field_validator
+
 
 class CVE(Model):
     id: str = Field(primary_field=True)
     cvss_score: float
     cvss_version: Literal["2.0", "3.0", "3.1"]
-    severity: Literal[1,2,3,4] = Field(default_factory=lambda: 1)
+    severity: Literal[1, 2, 3, 4] = Field(default_factory=lambda: 1)
 
-    model_config = {
-        "collection": "cves"
-    }
+    model_config = {"collection": "cves"}
 
     @field_validator("cvss_score")
     def validate_cvss_score(cls, v: float) -> float:
         if v < 0.0 or v > 10.0:
             raise ValueError("CVSS score must be between 0.0 and 10.0 inclusive")
         return v
-    
+
     def calculate_severity(self):
+        # Calculate severity from cvss on save
+        # Source: https://nvd.nist.gov/vuln-metrics/cvss
+        #
+        # Notes:
+        # - The CVSS score to severity mapping is not continuous (e.g. a
+        #   score of 8.95 is undefined according to their table).  However,
+        #   the CVSS equation documentation
+        #   (https://www.first.org/cvss/specification-document#CVSS-v3-1-Equations)
+        #   specifies that all CVSS scores are rounded up to the nearest tenth
+        #   of a point, so our severity mapping below is valid.
+        # - CVSSv3 specifies that a score of 0.0 has a severity of "None", but
+        #   we have chosen to map 0.0 to severity 1 ("Low") because CyHy code
+        #   has historically assumed severities between 1 and 4 (inclusive).
+        #   Since we have not seen CVSSv3 scores lower than 3.1, this will
+        #   hopefully never be an issue.
         if self.cvss_version == "2.0":
             if self.cvss_score == 10:
                 self.severity = 4
@@ -28,7 +45,7 @@ class CVE(Model):
                 self.severity = 2
             else:
                 self.severity = 1
-        else: # 3.0 or 3.1
+        else:  # 3.0 or 3.1
             if self.cvss_score >= 9.0:
                 self.severity = 4
             elif self.cvss_score >= 7.0:
--- tests/test_cve.py
+++ tests/test_cve.py
@@ -1,4 +1,7 @@
+# Third-Party Libraries
 import pytest
+
+# cisagov Libraries
 from cyhy_db.models.cve import CVE
 
 severity_params = [
@@ -16,20 +19,27 @@ severity_params = [
     ("3.1", 0.0, 1),
 ]
 
+
 @pytest.mark.parametrize("version, score, expected_severity", severity_params)
 def test_calculate_severity(version, score, expected_severity):
     cve = CVE(cvss_version=version, cvss_score=score, id="test-cve")
     cve.calculate_severity()
-    assert cve.severity == expected_severity, f"Failed for CVSS {version} with score {score}"
+    assert (
+        cve.severity == expected_severity
+    ), f"Failed for CVSS {version} with score {score}"
+
 
 def test_invalid_cvss_score():
-    with pytest.raises(ValueError, match="CVSS score must be between 0.0 and 10.0 inclusive"):
+    with pytest.raises(
+        ValueError, match="CVSS score must be between 0.0 and 10.0 inclusive"
+    ):
         CVE(cvss_version="3.1", cvss_score=11.0, id="test-cve")
+
 
 @pytest.mark.asyncio
 async def test_save(mongodb_engine):
-        cve = CVE(cvss_version="3.1", cvss_score=9.0, id="test-cve")
-        await cve.save(mongodb_engine)
-        saved_cve = await mongodb_engine.find_one(CVE, CVE.id == "test-cve")
-        assert saved_cve is not None, "CVE not saved correctly"
-        assert saved_cve.severity == 4, "Severity not calculated correctly on save"
+    cve = CVE(cvss_version="3.1", cvss_score=9.0, id="test-cve")
+    await cve.save(mongodb_engine)
+    saved_cve = await mongodb_engine.find_one(CVE, CVE.id == "test-cve")
+    assert saved_cve is not None, "CVE not saved correctly"
+    assert saved_cve.severity == 4, "Severity not calculated correctly on save"
